234dc2abd9de52e7e04287992cf7100d6ee7bee9
shon <shonbwd@gmail.com>
2023-01-14 20:55:52 +0530

    completed more assignments

--- 30-Question/script.js
+++ 30-Question/script.js
@@ -1,1 +1,28 @@
-// ? Create a simple calculator program in javascript which can perform the addition, substraction, multiplication, and division on given numbers.
+// program to create a simple calculator using the if else in javascript.
+// take the operator from the user through prompt box in javsscript
+const operator = prompt(
+  "Enter operator to perform the calculation(either +, -, * or /):"
+);
+
+// accept the number from the user through prompt box
+const number1 = parseFloat(prompt("Enter the first number"));
+const number2 = parseFloat(prompt("Enter the second number"));
+
+let result; // delcaration of the variable.
+
+// use it, elseif and else keyboard to define the calculator condition in Javascript.
+if (operator == "+") {
+  // use + (addition) operator to add two numbers
+  result = number1 + number2;
+} else if (operator == "-") {
+  // use - (subtraction) operator to subtract two numbers
+  result = number1 - number2;
+} else if (operator == "*") {
+  // use * (multiplication) operator to multiply two numbers
+  result = number1 * number2;
+} else {
+  result = number1 / number2; // use / (division) operator to divide two numbers
+}
+
+// display the result of the Calculator
+window.alert("Result is: " + result);
